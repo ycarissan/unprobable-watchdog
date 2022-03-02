01b694a5cbd685bd5a3f0649edf0f2d7777d687e
ycarissan <ycarissan@gmail.com>
2022-03-02 09:10:20 +0100

    messages MAJ

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+import os
 import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 def on_created(event):
-    print(f"hey, {event.src_path} has been created!")
+    print(f"{event.src_path} created")
 
 def on_deleted(event):
-    print(f"what the f**k! Someone deleted {event.src_path}!")
+    print(f"{event.src_path} deleted")
 
 def on_modified(event):
-    print(f"hey buddy, {event.src_path} has been modified")
+    size = os.path.getsize(event.src_path)
+    print(f"{event.src_path} new size : {size}")
 
 def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
-
+    print(f"{event.src_path} moved to {event.dest_path}")
 
 if __name__ == "__main__":
     patterns = ["*"]
@@ -34,9 +35,10 @@ if __name__ == "__main__":
     my_observer.schedule(my_event_handler, path, recursive=go_recursively)
 
     my_observer.start()
+
     try:
         while True:
-            time.sleep(1)
+            time.sleep(5)
     except KeyboardInterrupt:
         my_observer.stop()
         my_observer.join()
